62ed147da7da1d3f6fe7d45212a1d094a945ced2
Camoen <camoenvw@gmail.com>
2019-12-13 18:50:30 -0500

    add username input, public directory

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ const debug =require('debug')('server:debug');
 // import config from 'config';
 // import express from 'express';
 var config = require('config');
-// var express = require('express');
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').createServer(app);
 var socketio = require('socket.io')(http);
+var path = require('path')
 
+// app.use(express.static("src"));
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/client/html/index.html')
 });
@@ -16,9 +19,17 @@ socketio.on('connection', function(socket){
 	socket.on('disconnect', function(){
 		console.log('user disconnected');
 	});
-	socket.on('chat message', function(msg){
+	socket.on('chat-message', function(msg){
 		console.log('message: ' + msg);
-		socketio.emit('chat message', msg);
+		socketio.emit('chat-message', msg);
+	});
+	socket.on('user-typing', function(username){
+		console.log(username + " is typing");
+		socketio.emit('user-typing', username);
+	});
+	socket.on('stop-typing', function(username){
+		console.log(username + " stopped typing");
+		socketio.emit('stop-typing', username);
 	});
 });
 

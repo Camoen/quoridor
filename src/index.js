2ce2302983e8a2e69c8b6690a8604a3532f609da
Camoen <camoenvw@gmail.com>
2019-12-15 19:27:45 -0500

    Begin adding room functionality

--- src/index.js
+++ src/index.js
@@ -8,20 +8,20 @@ var http = require('http').createServer(app);
 var socketio = require('socket.io')(http);
 var path = require('path')
 
-// app.use(express.static("src"));
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/client/html/index.html')
 });
 
 var usernames = {};
-function mapToArray(usernames){
-	var usernameArray = [];
-	for (var key in usernames){
-		usernameArray.push(key);
-		// console.log( key, usernames[key] );
+var rooms = ["Room 1", "Room 2", "Room 3"];
+function mapToArray(inputMap){
+	var resultArray = [];
+	for (var key in inputMap){
+		resultArray.push(key);
+		// console.log( key, inputMap[key] );
 	}
-	return usernameArray;
+	return resultArray;
 }
 
 socketio.on('connection', function(socket){
@@ -29,26 +29,39 @@ socketio.on('connection', function(socket){
 	socket.on('user-created', (username)=>{
 		socket.username = username;
 		usernames[username] = username;
-		
+		socketio.to(socket.id).emit('update-room-list', rooms);
 		socketio.emit('update-user-list', mapToArray(usernames));
 	});
 	socket.on('disconnect', function(){
+		socketio.emit('chat-message', {
+			message: socket.username + " has disconnected.",
+			user: "System"
+		});
 		delete usernames[socket.username];
 		socketio.emit('update-user-list', mapToArray(usernames));
 		console.log('user disconnected');
 	});
 	socket.on('chat-message', function(msg){
 		console.log('message: ' + msg);
-		socketio.emit('chat-message', msg);
+		socketio.to(msg.room).emit('chat-message', msg);
 	});
-	socket.on('user-typing', function(username){
-		console.log(username + " is typing");
-		socketio.emit('user-typing', username);
+	socket.on('user-typing', function(userTyping){
+		console.log(userTyping.user + " is typing");
+		socketio.to(userTyping.room).emit('user-typing', userTyping.user);
 	});
-	socket.on('stop-typing', function(username){
-		console.log(username + " stopped typing");
-		socketio.emit('stop-typing', username);
+	socket.on('stop-typing', function(stopTyping){
+		console.log(stopTyping.user + " stopped typing");
+		socketio.to(stopTyping.room).emit('stop-typing', stopTyping.user);
 	});
+	socket.on('join-room', (room)=>{
+		if (socket.room){
+			console.log(socket.username + " leaving " + socket.room);
+			socket.leave(socket.room);
+		}
+		socket.room = room;
+		socket.join(room);
+		console.log(socket.username + " joining " + room);
+	})
 });
 
 http.listen((process.env.PORT || config.get('port')), function(){
